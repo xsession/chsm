92bdf84e0e0c30e3c83b9a9fb7f050dcde2e6f2c
Ivanyi Laszlo <k9eqto@gmail.com>
2023-02-12 20:26:58 +0100

    Update cmake_utils, expand vcnl4040 module, add jinja template files to modul template

--- modules/devices/proximity/vcnl4040/inc/vcnl4040_regs.h
+++ modules/devices/proximity/vcnl4040/inc/vcnl4040_regs.h
@@ -1,25 +1,25 @@
-#ifndef _ADAFRUIT_VCNL4040_H
-#define _ADAFRUIT_VCNL4040_H
+#ifndef VCNL4040_REGS_H
+#define VCNL4040_REGS_H
 
 #define VCNL4040_I2CADDR_DEFAULT 0x60 ///< VCNL4040 default i2c address
 
 // All addresses are for 16bit registers;
 // duplicates are for high or low bytes that aren't used together
-#define VCNL4040_ALS_CONFIG 0x00 ///< Ambient light sensor configuration register
-#define VCNL4040_ALS_THDH 0x01 ///< Ambient light high threshold register
-#define VCNL4040_ALS_THDL 0x02 ///< Ambient light low threshold register
-#define VCNL4040_PS_CONF1_L 0x03 ///< Proximity sensor configuration 1/2 register
-#define VCNL4040_PS_MS_H 0x04  ///< Proximity sensor configuration 1/2 register
-#define VCNL4040_PS_THDL 0x06  ///< Proximity sensor low threshold register
-#define VCNL4040_PS_THDH 0x07  ///< Proximity sensor high threshold register
-#define VCNL4040_PS_DATA 0x08  ///< Proximity sensor data register
-#define VCNL4040_ALS_DATA 0x09 ///< Ambient light sensor data register
-#define VCNL4040_WHITE_DATA 0x0A ///< White light sensor data register
-#define VCNL4040_INT_FLAG 0x0B   ///< Interrupt status register
-#define VCNL4040_DEVICE_ID 0x0C  ///< Device ID
+#define VCNL4040_ALS_CONFIG   0x00 ///< Ambient light sensor configuration register
+#define VCNL4040_ALS_THDH     0x01 ///< Ambient light high threshold register
+#define VCNL4040_ALS_THDL     0x02 ///< Ambient light low threshold register
+#define VCNL4040_PS_CONF1_L   0x03 ///< Proximity sensor configuration 1/2 register
+#define VCNL4040_PS_MS_H      0x04  ///< Proximity sensor configuration 1/2 register
+#define VCNL4040_PS_THDL      0x06  ///< Proximity sensor low threshold register
+#define VCNL4040_PS_THDH      0x07  ///< Proximity sensor high threshold register
+#define VCNL4040_PS_DATA      0x08  ///< Proximity sensor data register
+#define VCNL4040_ALS_DATA     0x09 ///< Ambient light sensor data register
+#define VCNL4040_WHITE_DATA   0x0A ///< White light sensor data register
+#define VCNL4040_INT_FLAG     0x0B   ///< Interrupt status register
+#define VCNL4040_DEVICE_ID    0x0C  ///< Device ID
 
-typedef enum led_current {
-  VCNL4040_LED_CURRENT_50MA,
+typedef enum {
+  VCNL4040_LED_CURRENT_50MA = 0,
   VCNL4040_LED_CURRENT_75MA,
   VCNL4040_LED_CURRENT_100MA,
   VCNL4040_LED_CURRENT_120MA,
@@ -27,24 +27,24 @@ typedef enum led_current {
   VCNL4040_LED_CURRENT_160MA,
   VCNL4040_LED_CURRENT_180MA,
   VCNL4040_LED_CURRENT_200MA,
-} VCNL4040_LEDCurrent;
+} vcnl4040_led_current_ten;
 
-typedef enum led_duty_cycle {
-  VCNL4040_LED_DUTY_1_40,
+typedef enum {
+  VCNL4040_LED_DUTY_1_40 = 0,
   VCNL4040_LED_DUTY_1_80,
   VCNL4040_LED_DUTY_1_160,
   VCNL4040_LED_DUTY_1_320,
-} VCNL4040_LEDDutyCycle;
+} vcnl4040_led_duty_cycle_ten;
 
-typedef enum ambient_integration_time {
-  VCNL4040_AMBIENT_INTEGRATION_TIME_80MS,
+typedef enum {
+  VCNL4040_AMBIENT_INTEGRATION_TIME_80MS = 0,
   VCNL4040_AMBIENT_INTEGRATION_TIME_160MS,
   VCNL4040_AMBIENT_INTEGRATION_TIME_320MS,
   VCNL4040_AMBIENT_INTEGRATION_TIME_640MS,
-} VCNL4040_AmbientIntegration;
+} vcnl4040_ambient_integration_time_ten;
 
-typedef enum proximity_integration_time {
-  VCNL4040_PROXIMITY_INTEGRATION_TIME_1T,
+typedef enum {
+  VCNL4040_PROXIMITY_INTEGRATION_TIME_1T = 0,
   VCNL4040_PROXIMITY_INTEGRATION_TIME_1_5T,
   VCNL4040_PROXIMITY_INTEGRATION_TIME_2T,
   VCNL4040_PROXIMITY_INTEGRATION_TIME_2_5T,
@@ -52,18 +52,20 @@ typedef enum proximity_integration_time {
   VCNL4040_PROXIMITY_INTEGRATION_TIME_3_5T,
   VCNL4040_PROXIMITY_INTEGRATION_TIME_4T,
   VCNL4040_PROXIMITY_INTEGRATION_TIME_8T,
-} VCNL4040_ProximityIntegration;
+} vcnl4040_proximity_integration_time_ten;
 
-typedef enum proximity_type {
-  VCNL4040_PROXIMITY_INT_DISABLE,
+typedef enum {
+  VCNL4040_PROXIMITY_INT_DISABLE = 0,
   VCNL4040_PROXIMITY_INT_CLOSE,
   VCNL4040_PROXIMITY_INT_AWAY,
   VCNL4040_PROXIMITY_INT_CLOSE_AWAY,
-} VCNL4040_ProximityType;
+} vcnl4040_proximity_type_ten;
 
-typedef enum interrupt_type {
+typedef enum {
   VCNL4040_PROXIMITY_AWAY,
   VCNL4040_PROXIMITY_CLOSE,
   VCNL4040_AMBIENT_HIGH = 4,
   VCNL4040_AMBIENT_LOW,
-} VCNL4040_InterruptType;
+} vcnl4040_interrupt_type_ten;
+
+#endif // VCNL4040_REGS_H
