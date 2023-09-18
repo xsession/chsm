80da878a50308dbb80105fe6c3114bdc9550520e
Ivanyi Laszlo <k9eqto@gmail.com>
2023-09-18 04:36:53 +0200

    Start to refactor new_module gui

--- gui/web/new_module/new_module.js
+++ gui/web/new_module/new_module.js
@@ -8,20 +8,8 @@ export class New_module{
         this.module_location = document.getElementById("module_location");
         this.browse_btn = document.getElementById("btn-new-module-loc-browse");
         this.browse_btn.addEventListener("click" , e => this.state_select("MODULE_BROWSE"));
-        // Batch
-        this.batch_cb = document.getElementById("batch_cb");
-        this.batch_default_loc_cb = document.getElementById("batch_default_loc_cb");
-        this.batch_location = document.getElementById("batch_location");
-        this.batch_browse_btn = document.getElementById("btn-batch-loc-browse");
-        this.batch_browse_btn.addEventListener("click" , e => this.state_select("BATCH_BROWSE"));
         // CMake
-        this.cmake_cb = document.getElementById("cmake_cb");
-        this.cmake_default_loc_cb = document.getElementById("cmake_default_cb");
-        this.cmake_location = document.getElementById("cmake_location");
         this.cmake_target_selector = document.getElementById("target-type-selector");
-        this.cmake_browse_btn = document.getElementById("btn-cmake-loc-browse");
-        this.cmake_browse_btn.addEventListener("click" , e => this.state_select("CMAKE_BROWSE"));
-
         // Buttons
         this.cancel_btn = document.getElementById("btn-cancel");
         this.cancel_btn.addEventListener("click" , e => this.state_select("CANCEL")); 
@@ -53,22 +41,18 @@ export class New_module{
                                     this.version.value, 
                                     this.description.value, 
                                     this.module_location.value, 
-                                    this.batch_location.value, 
+                                    this.linked_libs.value,
+                                    this.package_name.value,
                                     this.cmake_target_selector.value,
-                                    this.batch_cb.checked,
-                                    this.cmake_cb.checked);
+                                    this.device_address.value,
+                                    this.author.value,
+                                    this.full_name.value,
+                                    this.licence.value
+                                    );
                 console.log(this);
                 break;  
             case "MODULE_BROWSE":
                 this.browse_source = "MODULE_BROWSE"
-                eel.browse_dir_path();
-                break;
-            case "BATCH_BROWSE":
-                this.browse_source = "BATCH_BROWSE"
-                eel.browse_dir_path();
-                break;
-            case "CMAKE_BROWSE":
-                this.browse_source = "CMAKE_BROWSE"
                 eel.browse_dir_path();
                 break;
         }
@@ -90,13 +74,6 @@ function set_choosen_path_to_input_text(path)
             if(nm.cmake_default_loc_cb.checked)
                 nm.cmake_location.value = path;
             break;
-        case "BATCH_BROWSE":
-            nm.batch_location.value = path;
-            break;
-        case "CMAKE_BROWSE":
-            nm.cmake_location.value = path;
-            break;
-
     }
 }
 
